5e8d1b457502a0d6207fff0f5f4d8e2cb1e300b6
dx3mod <dx3mod@bk.ru>
2024-08-01 20:02:52 +0300

    Fix cache add_post

--- lib/posts_cache.ml
+++ lib/posts_cache.ml
@@ -8,11 +8,13 @@ and post = { vk_record_id : int; tg_message_id : int; last_modify : int }
 let load_from_file path = Sexplib.Sexp.load_sexp_conv_exn path t_of_sexp
 let save cache oc = Sexplib.Sexp.output_hum oc (sexp_of_t cache)
 let save_to_file ~path cache = Out_channel.with_open_text path (save cache)
-let add_post cache post = { cache with posts = post :: cache.posts }
+
+let add_post cache post =
+  { last_record_id = post.vk_record_id; posts = post :: cache.posts }
 
 let edit_post cache post =
   {
-    last_record_id = post.vk_record_id;
+    cache with
     posts =
       List.map
         (fun p -> if p.vk_record_id = post.vk_record_id then post else p)
@@ -21,4 +23,3 @@ let edit_post cache post =
 
 let iter f cache = List.iter f cache.posts
 let posts cache = cache.posts
-let last_post cache = List.hd cache.posts
