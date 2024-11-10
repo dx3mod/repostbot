9cddb86da34029872f4812388d2a6a8a2cb1fd0b
dx3mod <dx3mod@bk.ru>
2024-11-10 17:18:54 +0300

    Add update edited posts

--- bin/main.ml
+++ bin/main.ml
@@ -13,7 +13,7 @@ module Repost_bot (Depends : Repost_bot_depends) = struct
 
   let pull_new_and_edited_vk_posts () =
     let%lwt Vkashka.Wall.{ items = posts; _ } =
-      Vk_api.Wall.get (`Domain project_env.targets.vk_user)
+      Vk_api.Wall.get ~count:"30" (`Domain project_env.targets.vk_user)
     in
 
     let new_posts =
@@ -92,10 +92,40 @@ module Repost_bot (Depends : Repost_bot_depends) = struct
 
     Lwt.return message
 
+  let edit_post ~(cached_post : Posts_cache.post)
+      ~(vk_post : Vkashka.Wall.Record.t) =
+    let format_unix_time time =
+      time |> float_of_int |> Unix.localtime |> fun t ->
+      Printf.sprintf "%04d-%02d-%02d %02d:%02d" (t.tm_year + 1900)
+        (t.tm_mon + 1) t.tm_mday t.tm_hour t.tm_min
+    in
+
+    let%lwt _ =
+      Tg_bot_api.edit_message_text ~chat_id:project_env.targets.tg_chat_id
+        ~message_id:cached_post.tg_message_id
+      @@ Printf.sprintf "%s\n\nedited at %s" (crop_text vk_post.text)
+           (format_unix_time @@ Utils.last_date_vk_record vk_post)
+    in
+    Lwt.return_unit
+
   let start () =
     Lwt_io.printlf "Start reposting";%lwt
 
-    let%lwt new_posts, _ = pull_new_and_edited_vk_posts () in
+    let%lwt new_posts, edited_posts = pull_new_and_edited_vk_posts () in
+
+    Lwt_list.iter_p
+      (fun (vk_post, cached_post) ->
+        try%lwt
+          edit_post ~vk_post ~cached_post;%lwt
+
+          Posts_cache.edit_post cache
+            { cached_post with last_modify = Utils.last_date_vk_record vk_post };
+
+          Lwt.return_unit
+        with _ ->
+          Lwt_io.printlf "Failed to update post vk_id:%d!"
+            cached_post.vk_record_id)
+      edited_posts;%lwt
 
     Lwt_list.iter_s
       (fun vk_post ->
